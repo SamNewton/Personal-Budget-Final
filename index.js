ed609131bab258705c73b07d77d46c54ea7dc3c3
SamNewton <sam@samnewton.net>
2020-12-16 13:47:31 -0500

    final

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+ const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 80;
 const bodyParser = require("body-parser");
 const fs = require("fs");
+
+// Using json files as sort of "database"
+// Easier to manage smaller amounts of data like this
 var users = require("./users.json");
 var budgets = require("./budgets.json");
 
@@ -13,12 +15,20 @@ app.use('/', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(port, () => {
-    console.log(`Served at http://localhost:${port}`);
+app.listen(80, () => {
+    console.log('Express served at 67.205.161.184');
 });
 
 app.post('/register', (req, res) => {
     const { username, password } = req.body;
+
+    if(username.length < 3) {
+        res.send({success: false, message: "Your username must be at least 3 characters long."});
+    } 
+
+    if(password.length < 6) {
+        res.send({success: false, message: "Your password must be at least 6 characters long."});
+    }
 
     if(users[username] != null) {
         res.send({success: false, message: "That username is already taken"});
@@ -31,7 +41,6 @@ app.post('/register', (req, res) => {
         res.send({success: true, user: username,auth: users[username]["auth"]});
     }
 
-  
 });
 
 app.post('/login', (req, res) => {
@@ -47,7 +56,6 @@ app.post('/login', (req, res) => {
     } else {
         res.send({success: false, message: "Incorrect login information"});
     }
-    
 });
 
 
@@ -71,17 +79,18 @@ app.post('/addExpense', (req, res) => {
     if(users[user]["auth"] == auth) {
         budgets[user][title]["spent"] += Number(spent);
         updateFiles();
-        res.send({});
     }
+    res.send({});
 });
 
 app.post('/editBudget', (req, res) => {
     const { user, auth, title, amount } = req.body;
     if(users[user]["auth"] == auth) {
         budgets[user][title]["amt"] = Number(amount);
-        updateFiles();
-        res.send({});
+        updateFiles(); 
     }
+
+    res.send({});
 });
 
 app.post('/deleteBudget', (req, res) => {
@@ -92,13 +101,19 @@ app.post('/deleteBudget', (req, res) => {
         }
         delete budgets[user][title];
         updateFiles();
-        res.send({});
     }
+
+    res.send({});
 });
 
 app.post('/getBudgets', (req, res) => {
     const { user, auth } = req.body;
-    res.send(budgets[user]);
+    if(budgets[user] != null) {
+        res.send(budgets[user]);
+    }
+
+    res.send({});
+    
 });
 
 
